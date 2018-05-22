011c296e8d754bdf51203f0e2ae01027a53a12d7
robilau <robilau69@gmail.com>
2018-05-22 11:39:15 -0300

    classe DAO generica iniciada

--- creche/src/main/java/Infraestrutura/DAO.java
+++ creche/src/main/java/Infraestrutura/DAO.java
@@ -5,10 +5,28 @@
  */
 package Infraestrutura;
 
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
 /**
- *
- * @author Aluno
+ * Classe que faz a conexão com o banco de dados
+ * @author Leticia
  */
-public class DAO {
+public class DAO <T>{
+    DB _db;
+    Jongo _jongo;
+    MongoCollection _colecao;
+
+    public DAO(String colecao) {
+        _db = new Mongo().getDB("Creche");
+        _jongo = new Jongo(_db);
+        _colecao = _jongo.getCollection(colecao);
+    }
+    
+    public Iterable<T> PegarTodos(){
+        return _colecao.find().as(clazz);
+    }
     
 }
