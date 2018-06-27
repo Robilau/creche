ec9358a1df46482c1e3370f25f25769828857704
Gabrielce208 <jogoscelestino@gmail.com>
2018-06-26 23:07:09 -0300

    os titulos devem ser alterados provavelmente

--- creche/src/main/java/Apresentacao/Base/FramePrincipal.java
+++ creche/src/main/java/Apresentacao/Base/FramePrincipal.java
@@ -61,6 +61,7 @@ public class FramePrincipal extends javax.swing.JFrame {
     private FrameGerenciadorCadastroRpc frameGerenciadorCadastroRpc;
     private FrameGerenciadorCadastroTurma frameGerenciadorCadastroTurma;
     private FrameLogin frameLogin;
+    private FrameInformacoesDetalhadasCrianca frameInformacoesDetalhadasCrianca ;
 
     public FramePrincipal() {
         iniciarServicos();
@@ -116,6 +117,7 @@ public class FramePrincipal extends javax.swing.JFrame {
         frameGerenciadorCadastroRpc = new FrameGerenciadorCadastroRpc(rpcService);
         frameGerenciadorCadastroTurma = new FrameGerenciadorCadastroTurma(cuidadorService, turmaService);
         frameLogin = new FrameLogin(loginService);
+        frameInformacoesDetalhadasCrianca= new FrameInformacoesDetalhadasCrianca();
     }
 
     public void iniciarServicos() {
@@ -163,6 +165,7 @@ public class FramePrincipal extends javax.swing.JFrame {
         jMenuCuidador = new javax.swing.JMenuItem();
         jMenuRpc = new javax.swing.JMenuItem();
         jMenuTurma = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
         menuUsuario = new javax.swing.JMenu();
         jMenuSenha = new javax.swing.JMenuItem();
         jMenuLogout = new javax.swing.JMenuItem();
@@ -258,6 +261,15 @@ public class FramePrincipal extends javax.swing.JFrame {
         });
         menuContexto.add(jMenuTurma);
 
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F5, 0));
+        jMenuItem1.setText("Relatório");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        menuContexto.add(jMenuItem1);
+
         jMenuBar2.add(menuContexto);
 
         menuUsuario.setText("Usuario");
@@ -332,6 +344,11 @@ public class FramePrincipal extends javax.swing.JFrame {
         adicionaTela(frameGerenciadorCadastroTurma, true);
     }//GEN-LAST:event_jMenuTurmaActionPerformed
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+       fecharTodosFrames();
+        adicionaTela(frameInformacoesDetalhadasCrianca, true);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -376,6 +393,7 @@ public class FramePrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar2;
     private static javax.swing.JMenuItem jMenuCrianca;
     private static javax.swing.JMenuItem jMenuCuidador;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuLogout;
     private static javax.swing.JMenuItem jMenuRpc;
